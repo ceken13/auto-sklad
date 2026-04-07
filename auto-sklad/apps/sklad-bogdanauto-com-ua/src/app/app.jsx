4a5e396321b24c16657a468eeddfe24617b616fc
Kateryna Shcheholieva <katynchik.994.mail@gmail.com>
2026-04-07 17:05:50 +0200

    add fake Authorization, add rout for AuthorizationPage and UserPage

--- auto-sklad/apps/sklad-bogdanauto-com-ua/src/app/app.jsx
+++ auto-sklad/apps/sklad-bogdanauto-com-ua/src/app/app.jsx
@@ -1,7 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
 import { CarsDetails } from '../pages/CarsDetails';
 import { CarsRequest } from '../pages/CarsRequest';
+import { UserAuthorizationPage } from '../pages/UserAuthorizationPage';
+import { UserPage } from '../pages/UserPage';
+import { PrivateRoute } from '../components/PrivateRoute';
+
 export function App() {
   return (
     <div>
@@ -9,6 +13,16 @@ export function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/car-details/:id" element={<CarsDetails />} />
         <Route path="/car-request/:id" element={<CarsRequest />} />
+        <Route path="/login" element={<UserAuthorizationPage />} />
+
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <UserPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
